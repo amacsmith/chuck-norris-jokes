6115233f41e5c5c6e2432b701819278f8eda5ae7
amacsmith <alex.macsmith@gmail.com>
2019-11-16 10:01:06 +0000

    Apply fixes from StyleCI

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -1,4 +1,6 @@
-<?php /** @noinspection ALL */
+<?php
+
+/** @noinspection ALL */
 
 namespace AMacSmith\ChuckNorrisJokes\Tests;
 
@@ -229,17 +231,16 @@ class JokeFactoryTest extends TestCase
 
         $this->assertCount(5, $jokes);
 
-        foreach($jokes as $joke){
+        foreach ($jokes as $joke) {
             $this->assertContains($joke, $this->chuckNorrisjokes);
         }
-
     }
 
     /** @test */
     public function the_number_of_jokes_requested_cant_be_negative()
     {
         $this->expectExceptionCode(500);
-        $this->expectErrorMessage("getRandomJokes requires a integer greater than 0. -5 was used for \$count argument.");
+        $this->expectErrorMessage('getRandomJokes requires a integer greater than 0. -5 was used for $count argument.');
 
         $jokes = new JokeFactory();
         $jokes = $jokes->getRandomJokes(-5);
